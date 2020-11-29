aa0130e9ecefb7e596239e7f0b1d471b7c421f6a
jchen <jchen.6727@gmail.com>
2020-11-28 20:00:09 -0500

    generalization for channel

--- test.py
+++ test.py
@@ -8,9 +8,10 @@ import genrn
 
 h.celsius = 21
 
-mechs = {'a1p7': {} 'b1p7': {} 'c1p7', 'a1p8', 'b1p8', 'c1p8']
-soma = genrn( h=h, mechs = )
+soma = h.Section()
 
+def getval( section, mech, var ):
+    return section.__getattribute__(mech).__getattribute__(var)
 
 chans = {'nav1.7': ['a1p7', 'b1p7', 'c1p7'], 'nav1.8': ['a1p8', 'b1p8', 'c1p8']}
 traces = ['minf', 'mtau', 'hinf', 'htau']
@@ -27,7 +28,7 @@ for nav in chans:
 vs = np.linspace(-70,30,100)
 for v in vs:
     h.finitialize(v)
-    data['a1p7']['minf'].append(soma(0.5).a1p7.minf)
+    data['a1p7']['minf'].append(getval( soma(0.5), 'a1p7', 'minf')
     data['a1p7']['mtau'].append(soma(0.5).a1p7.mtau)
     data['a1p7']['hinf'].append(soma(0.5).a1p7.hinf)
     data['a1p7']['htau'].append(soma(0.5).a1p7.htau)
@@ -56,11 +57,11 @@ for v in vs:
     data['c1p8']['mtau'].append(soma(0.5).c1p8.tau_m)
     data['c1p8']['hinf'].append(soma(0.5).c1p8.hinf)
     data['c1p8']['htau'].append(soma(0.5).c1p8.tau_h)
-
-for trace in traces:
-    for nav in chans:
-        plt.title(nav + " " + trace)
-        for chan in chans[nav]:
-            plt.plot(vs, data[chan][trace], label=chan)
-        plt.legend()
-        plt.show()
+#
+#for trace in traces:
+#    for nav in chans:
+#        plt.title(nav + " " + trace)
+#        for chan in chans[nav]:
+#            plt.plot(vs, data[chan][trace], label=chan)
+#        plt.legend()
+#        plt.show()
